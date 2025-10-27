b4ff9d288ee13e250699697d4ddb4339faead70f
Petter55 <e.petter@hotmail.com>
2025-10-26 22:06:44 -0300

    users count

--- count-humans-query.sql
+++ count-humans-query.sql
@@ -1,15 +1,9 @@
--- pc1 busca commits que tenham arquivos de teste e que nao sejam feitos por bots
--- na mesma tabela, pc2 busca commits com filenames referentes aos arquivos testados e que sejam feitos por bots
--- verifica que pc1 tenha sido criado em uma PR posterior ao fechamento da PR de pc2 OU que façam parte da mesma PR
--- WITH r_test AS (
-  SELECT DISTINCT (pc1.sha) as human_commit_sha, pc2.sha as bot_commit_sha, pc1.pr_id, pc1.patch, pc1.committer as human_committer, pc2.committer as bot_commiter, pr1.html_url as human_html_url, pr1.repo_url, pr2.html_url as bot_html_url
+SELECT COUNT(DISTINCT (pc1.committer))
   FROM pr_commit_details pc1, pr_commit_details pc2
   INNER JOIN pull_request pr1
   ON pc1.pr_id = pr1.id
   INNER JOIN pull_request pr2
   ON pc2.pr_id = pr2.id
-  -- INNER JOIN user
-  -- ON pc1.committer = user.login -- timeout, falta um index no schema
   WHERE
     (
       pc1.filename LIKE '%test.ts'
@@ -38,13 +32,13 @@
       OR pc1.pr_id == pc2.pr_id --falta um campo de commit timestamp no schema
       )
     AND (
-    pc2.committer LIKE '%[bot]'
-    OR
-    pc2.committer == 'cursoragent'
-    OR
-    pc2.committer == 'Claude Bot'
-    OR
-    pc2.committer == 'Copilot'
+      pc2.committer LIKE '%[bot]'
+      OR
+      pc2.committer == 'cursoragent'
+      OR
+      pc2.committer == 'Claude Bot'
+      OR
+      pc2.committer == 'Copilot'
     )
     AND (
         pc2.filename == replace(pc1.filename, 'tsx', 'test.tsx')
@@ -55,5 +49,4 @@
         OR
         pc2.filename == replace(pc1.filename, 'ts', 'spec.ts')
     )
--- )
-
+--result: 21
